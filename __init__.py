4d97dddc10b0d6db02920333937d316f9bea9bbf
elKuston <romolocaponera@uma.es>
2023-05-17 12:53:19 +0200

    V2
    -Faster loading times, due to loading only subjects that have been studied (i.e. the lesson has been completed) since last sync, rather than loading all subjects that have changed (which includes reviewed items).
    -Fixed a bug that caused crash with some vocabulary that has no prununciation audio
    -New card model that includes "parts of speech"

--- __init__.py
+++ __init__.py
@@ -11,21 +11,24 @@ config = mw.addonManager.getConfig(__name__)
 headers = {'Authorization': 'Bearer '+config['API_KEY']}
 include_audio = config['include_audio']
 deckName = config['deck_name']
-modelName = 'WaniKaniVocabAddOn'
+modelName = 'WaniKaniVocabAddOnV2'
 
 
 # WaniKani API part
-
+def request_get(url, headers=headers):
+    return requests.get(url, headers=headers)
 
 
 def load_subjects(url):
-    response = requests.get(url, headers=headers)
+    print(url)
+    response = request_get(url)
     if response.status_code == 422:
         print('Data is still fresh, not refreshing')
         return []
     else:
         result = response.json()
-        subject_ids = list(map(lambda data: data['data']['subject_id'], result['data']))
+        filtered = filter(lambda data: datetime.datetime.fromisoformat(data['data']['started_at'][:-1]) > datetime.datetime.fromisoformat(config['last_sync']), result['data'])
+        subject_ids = list(map(lambda filtered_datadata: filtered_datadata['data']['subject_id'], filtered))
         next_url = result['pages']['next_url']
         if next_url is not None:
             subject_ids.extend(load_subjects(next_url))
@@ -33,14 +36,15 @@ def load_subjects(url):
 
 
 def map_subject(subject_id):
-    response = requests.get('https://api.wanikani.com/v2/subjects/' + str(subject_id), headers=headers)
+    response = request_get('https://api.wanikani.com/v2/subjects/' + str(subject_id))
     while response.status_code == 429:
         current_time = time.time()
-        reset_timestamp = float(response.headers['RateLimit-Reset'])
-        wait_time = reset_timestamp - current_time
-        print('going to sleep for ', (wait_time + 5), 'seconds, waiting for request limit to reset')
-        time.sleep(wait_time + 5)
-        response = requests.get('https://api.wanikani.com/v2/subjects/' + str(subject_id), headers=headers)
+        reset_timestamp = float(response.headers['Ratelimit-Reset'])
+        print('current time:',current_time,'ratelimit reset time:', reset_timestamp)
+        wait_time = max(0, reset_timestamp - current_time)
+        print('going to sleep for ', (wait_time), 'seconds, waiting for request limit to reset')
+        time.sleep(wait_time)
+        response = request_get('https://api.wanikani.com/v2/subjects/' + str(subject_id))
 
     subject = response.json()
     sub_data = subject['data']
@@ -49,7 +53,8 @@ def map_subject(subject_id):
             'meanings': get_meanings(sub_data),
             'readings': get_readings(sub_data),
             'audio': get_audio(sub_data),
-            'subject_id': subject_id}
+            'subject_id': subject_id,
+            'parts_of_speech':sub_data['parts_of_speech']}
 
 
 def get_meanings(sub_data):
@@ -66,7 +71,10 @@ def get_readings(sub_data):
 
 
 def get_audio(sub_data):
-    return sub_data['pronunciation_audios'][0]['url']
+    if len(sub_data['pronunciation_audios'])>0:
+        return sub_data['pronunciation_audios'][0]['url']
+    else:
+        return None
 
 
 
@@ -97,8 +105,9 @@ def subject_to_anki_note(col, deck_id, model, subject):
     anki_note['Kanji'] = subject['characters']
     anki_note['Reading'] = readings
     anki_note['WaniKaniSubjectId'] = str(subject['subject_id'])
+    anki_note['PartsOfSpeech'] = ', '.join(subject['parts_of_speech'])
 
-    if include_audio:
+    if include_audio and subject['audio'] is not None:
         audio_filename = subject['characters']+'.mp3'
         download(col, audio_filename, subject['audio'])
         anki_note['Pronunciation'] += u'[sound:{}]'.format(audio_filename)
@@ -107,8 +116,8 @@ def subject_to_anki_note(col, deck_id, model, subject):
 
 def is_duplicate(col, deck_id, new_note):
     csum = anki.utils.field_checksum(new_note['WaniKaniSubjectId'])
-    query = 'select n.id from notes n join cards c on n.id = c.nid where n.csum=? and c.did=?'
-    queryArgs = [csum, deck_id]
+    query = 'select n.id from notes n  where n.csum=?'
+    queryArgs = [csum]
     return len(list(map(lambda id: col.get_note(id), col.db.list(query, *queryArgs)))) > 0
 
 
@@ -122,10 +131,11 @@ def create_model_if_not_exists(col):
         models.add_field(m, models.new_field('Kanji'))
         models.add_field(m, models.new_field('Reading'))
         models.add_field(m, models.new_field('Pronunciation'))
+        models.add_field(m, models.new_field('PartsOfSpeech'))
 
         template = models.new_template('sampleWKTemplate')
         template['qfmt'] = '{{Meanings}}'
-        template['afmt'] = '{{Meanings}}<hr>{{Kanji}}<hr>{{Reading}}<hr>{{Pronunciation}}'
+        template['afmt'] = '{{Meanings}}<hr>{{Kanji}}<hr>{{Reading}}<hr>{{Pronunciation}}<hr>{{PartsOfSpeech}}'
         models.addTemplate(m, template)
         models.add(m)
 
@@ -138,10 +148,17 @@ def add_cards_anki(col, subjects_mapped):
     model = col.models.by_name(modelName)
 
     #mw.requireReset()
-    for subject in subjects_mapped:
+    for i, subject in enumerate(subjects_mapped):
         anki_note = subject_to_anki_note(col, deck['id'], model, subject)
         if not is_duplicate(col, deck['id'], anki_note):
             col.addNote(anki_note)
+            aqt.mw.taskman.run_on_main(
+                lambda: aqt.mw.progress.update(
+                    label=f"Inserting vocabulary cards ({i}/{len(subjects_mapped)})",
+                    value=i,
+                    max=len(subjects_mapped),
+                )
+            )
     col.autosave()
     #mw.maybeReset()
 
@@ -155,15 +172,13 @@ def import_vocab_from_wanikani(col):
     all_subjects = load_subjects(
         'https://api.wanikani.com/v2/assignments?started=true&subject_types=vocabulary&updated_after=' + str(
             config['last_sync']))
-
+    print('loaded subjects:', all_subjects)
     all_subjects_mapped = []
-    i=0
-    for subject in all_subjects:
+    for i, subject in enumerate(all_subjects):
         all_subjects_mapped.append(map_subject(subject))
-        i+=1
         aqt.mw.taskman.run_on_main(
             lambda: aqt.mw.progress.update(
-                label=f"Loading WaniKani vocabulary: {all_subjects_mapped[-1]['characters']} ({i}/{len(all_subjects)})",
+                label=f"Downloading WaniKani vocabulary: {all_subjects_mapped[-1]['characters']} ({i}/{len(all_subjects)})",
                 value=i,
                 max=len(all_subjects),
             )
@@ -174,7 +189,6 @@ def import_vocab_from_wanikani(col):
 # MAIN CODE
 
 def main():
-
     QueryOp(
         parent=mw,
         op=import_vocab_from_wanikani,
